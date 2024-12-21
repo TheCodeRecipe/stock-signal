8fb2cdc9edc26ac2e217b0b027ab3e244cf2c4e6
TheCodeRecipe <thecoderecipe@gmail.com>
2024-12-21 23:44:07 +0900

    업데이트 추가

---
+++ app/action/update.ts
@@ -0,0 +1,40 @@
+'use server';
+
+import { cookies } from "next/headers";
+
+const API_BASE_URL = process.env.NEXT_API_BASE_URL || "";
+
+// 주식 데이터 업데이트 함수
+export async function updateStocks(): Promise<{ success: boolean; message: string }> {
+  try {
+    const cookieStore = await cookies(); // 서버에서 쿠키 접근
+    const token = cookieStore.get("accessToken")?.value;
+    if (!token) return  {
+      success: false,
+      message: "업데이트 중 오류가 발생했습니다.",
+    };
+
+    const response = await fetch(`${API_BASE_URL}/update-stocks`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`, // 헤더에 토큰 추가
+      },
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.error || "업데이트 실패");
+    }
+
+    return {
+      success: true,
+      message: "한국 주식 업데이트 완료!",
+    };
+  } catch (error) {
+    console.error('Error updating stocks:', error);
+    return {
+      success: false,
+      message: "업데이트 중 오류가 발생했습니다.",
+    };
+  }
+}
